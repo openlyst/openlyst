e5a74a8733daf2545f1c87970463d44f8763659c
HttpAnimations <shawndouglasyt@gmail.com>
2025-10-21 21:36:47 -0700

    Refactor index.ts to export components for easier imports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,8 @@
 // place files you want to import through the `$lib` alias in this folder.
+
+// Export components
+export { default as Header } from './components/Header.svelte';
+export { default as Footer } from './components/Footer.svelte';
+export { default as AppCard } from './components/AppCard.svelte';
+export { default as Section } from './components/Section.svelte';
+export { default as Button } from './components/Button.svelte';
