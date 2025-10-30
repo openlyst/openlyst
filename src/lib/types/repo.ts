421e8e867c524594725011977bb0d94edb395485
HttpAnimations <shawndouglasyt@gmail.com>
2025-10-29 17:16:24 -0700

    Add type definitions for app-related structures in repo.ts

---
+++ src/lib/types/repo.ts
@@ -0,0 +1,75 @@
+export interface Screenshot {
+  imageURL?: string;
+  width?: number;
+  height?: number;
+}
+
+export interface PlatformInstall {
+  iOS?: string;
+  Android?: string;
+  macOS?: string;
+  Windows?: string;
+  Linux?: string;
+}
+
+export interface AppVersion {
+  version: string;
+  platformInstall: PlatformInstall;
+  localizedDescription: string;
+  date?: string;
+  downloadURL?: string;
+  size?: number;
+  minOSVersion?: string;
+}
+
+export interface AppScreenshots {
+  iphone?: (string | Screenshot)[];
+  ipad?: (string | Screenshot)[];
+  appleTV?: (string | Screenshot)[];
+  [key: string]: (string | Screenshot)[] | undefined;
+}
+
+export interface App {
+  name: string;
+  bundleIdentifier?: string;
+  subtitle: string;
+  localizedDescription: string;
+  iconURL: string;
+  tintColor: string;
+  platforms: string[];
+  screenshots: (string | Screenshot)[] | AppScreenshots;
+  versions: AppVersion[];
+  appPermissions?: {
+    entitlements?: string[];
+    privacy?: string[];
+  };
+  beta?: boolean;
+  patreon?: {
+    pledgeURL: string;
+  };
+}
+
+export interface NewsItem {
+  title: string;
+  identifier: string;
+  caption: string;
+  date: string;
+  tintColor: string;
+  imageURL?: string;
+  notify: boolean;
+  url?: string;
+  appID?: string;
+}
+
+export interface RepoConfig {
+  name: string;
+  subtitle: string;
+  description: string;
+  iconURL: string;
+  headerURL?: string;
+  website: string;
+  tintColor: string;
+  featuredApps: string[];
+  apps: App[];
+  news: NewsItem[];
+}
