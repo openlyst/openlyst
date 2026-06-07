31080fb6702724823f6e61258ca4282309184f5a
HttpAnimations <shawndouglasyt@gmail.com>
2026-06-07 10:14:45 -0700

    iinit

--- src/app/about/page.tsx
+++
@@ -1,8 +0,0 @@
-import { Section, Button } from '@/components';
-import { AboutContent } from '@/components/AboutContent';
-
-export const dynamic = 'force-dynamic';
-
-export default function AboutPage() {
-  return <AboutContent />;
-}
--- src/app/docs/api/page.tsx
+++
@@ -1,7 +0,0 @@
-import { DocsApiContent } from '@/components/DocsApiContent';
-
-export const dynamic = 'force-dynamic';
-
-export default function DocsApiPage() {
-  return <DocsApiContent />;
-}
